663fb5e1a8bc03908652507b39af0a4706a0a913
Granth Sharma <77103544+Granth-Sharma@users.noreply.github.com>
2023-06-02 11:55:58 +0530

    Guess The Number.py

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,19 +1,18 @@
 from random import randint
 num=randint(1,100)
-
-print("WELCOME TO GUESS ME!")
-print("I'm thinking of a number between 1 and 100")
-print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
-print("If your guess is within 10 of my number, I'll tell you you're WARM")
-print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
-print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
-print("LET'S PLAY!")
-
+print('''
+WELCOME TO GUESS ME!
+I'm thinking of a number between 1 and 100
+If your guess is more than 10 away from my number, I'll tell you you're COLD
+If your guess is within 10 of my number, I'll tell you you're WARM
+If your guess is farther than your most recent guess, I'll say you're getting COLDER
+If your guess is closer than your most recent guess, I'll say you're getting WARMER
+LET'S PLAY!
+''')
 guesses = [0]
 
 while True:
-
-    # we can copy the code from above to take an input
+    
     guess = int(input("I'm thinking of a number between 1 and 100.\n  What is your guess? "))
     
     if guess < 1 or guess > 100:
